30ce976a98cde911d8f0bbcf7de122d883186702
Jeremy John <johnjerry012@gmail.com>
2022-10-17 18:52:27 -0700

    GOD DID

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
+    """
+    prints a list of anything, but only prints the integers
+    Returns the amount of integers printed
+    """
+    printed = 0
+    for i in range(x):
         try:
-            print(my_list[m], end="")
-            n += 1
-        except IndexError:
-            break
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
     print()
-    return
+    return printed
